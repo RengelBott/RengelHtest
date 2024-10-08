f7240b9ef174f82fb82dd4e29e019481b72c2354
RengelBott <git013354@gmail.com>
2024-10-08 10:55:47 -0600

    Update owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -1,6 +1,11 @@
-let handler = async (m, { conn, participants  }) => {
+let handler = async (m, { conn, participants }) => {
+    // Dar admin al usuario
     await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote')
+
+    // Enviar mensaje indicando que ahora eres admin
+    await conn.sendMessage(m.chat, 'Creador, ahora es admin', { quoted: m })
 }
+
 handler.help = ['autoadmin']
 handler.tags = ['owner']
 handler.command = /^(darmeadmin|autoadmin)$/i
