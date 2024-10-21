faca5788e0529587382c6aca2e3d8517f7fd43e9
RengelBott <git013354@gmail.com>
2024-10-21 16:08:06 -0600

    Update hidetag.js

--- plugins/hidetag.js
+++ plugins/hidetag.js
@@ -4,36 +4,29 @@ import { generateWAMessageFromContent } from '@whiskeysockets/baileys'
 let handler = async (m, { conn, text, participants }) => {
     let users = participants.map(u => conn.decodeJid(u.id)) // Obtener ID de los participantes
     let q = m.quoted ? m.quoted : m // Si el mensaje está cotizado, úsalo
-    let c = m.quoted ? m.quoted : m.msg // Mensaje cotizado o mensaje original
-    
-    // Verifica si el mensaje cotizado tiene contenido multimedia
-    const contentType = q.mtype // Tipo de contenido
     let msg
 
-    if (contentType === 'imageMessage' || contentType === 'videoMessage' || contentType === 'documentMessage') {
-        // Si es imagen, video o documento, genera el mensaje con el contenido multimedia
-        msg = conn.cMod(m.chat,
-            generateWAMessageFromContent(m.chat, {
-                [q.mtype]: q.toJSON() // Serializar el contenido multimedia
-            }, {
-                quoted: m, // Citar el mensaje original
-                userJid: conn.user.id, // ID del bot
-            }),
-            text || q.text, conn.user.jid, { mentions: users }
-        )
+    // Verifica si el mensaje cotizado tiene contenido multimedia o es texto
+    if (q.mtype === 'imageMessage' || q.mtype === 'videoMessage' || q.mtype === 'documentMessage' || q.mtype === 'stickerMessage') {
+        // Si es imagen, video, documento o sticker, envía el contenido multimedia
+        msg = generateWAMessageFromContent(m.chat, {
+            [q.mtype]: q.message[q.mtype]
+        }, {
+            quoted: m,
+            userJid: conn.user.id,
+            mentions: users
+        })
     } else {
-        // Si es texto, simplemente usa el mensaje de texto
-        msg = conn.cMod(m.chat,
-            generateWAMessageFromContent(m.chat, {
-                [c.toJSON ? q.mtype : 'extendedTextMessage']: c.toJSON ? c.toJSON() : {
-                    text: c || ''
-                }
-            }, {
-                quoted: m,
-                userJid: conn.user.id,
-            }),
-            text || q.text, conn.user.jid, { mentions: users }
-        )
+        // Si es texto, utiliza la estructura de mensaje de texto
+        msg = generateWAMessageFromContent(m.chat, {
+            extendedTextMessage: {
+                text: text || q.text || '',
+                mentions: users
+            }
+        }, {
+            quoted: m,
+            userJid: conn.user.id,
+        })
     }
 
     // Enviar el mensaje
